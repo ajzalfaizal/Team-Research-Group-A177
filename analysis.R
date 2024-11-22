58ec7df44d29f9cbf74b3db224eb0d548c4bc10f
gshankar631 <gk24aav@herts.ac.uk>
2024-11-22 19:44:41 +0000

    analysis of s,pvalue,alt hypothesis

---
+++ analysis.R
@@ -0,0 +1,15 @@
+# Load necessary library
+library(readr)
+
+
+data <-read.csv("C:/Users/user/OneDrive/Desktop/Womens Clothing E-Commerce Reviews.csv")
+
+
+# Spearman's Rho test for correlation between two variables
+result <- cor.test(data$Rating, data$Age, method = "spearman")
+
+
+# Print the test results
+print(result)
+
+
