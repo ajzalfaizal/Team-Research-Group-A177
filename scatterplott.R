af21af885b6938d2154381d769964e0c93324a33
gshankar631 <gk24aav@herts.ac.uk>
2024-11-22 15:54:06 +0000

    scatterplot

---
+++ scatterplott.R
@@ -0,0 +1,23 @@
+library(readr)
+
+data <- read.csv("C:/Users/user/OneDrive/Desktop/Womens Clothing E-Commerce Reviews.csv")
+
+df<-data
+
+x <- df$Age
+y <- df$Rating
+
+
+# Plot with main and axis titles
+# Change point shape (pch = 19) and include frame.
+plot(x   # independent variable (Age)
+   , y # dependent variable (Rating)
+  , main = "Scatterplot of Rating vs Age" # chart title
+  , xlab = "Age" # x-axis label
+  , ylab = "Rating" # y-axis label
+  , pch = 19 # point shape (filled circle)
+  , frame = T # surround chart with a frame
+       )
+model <- lm(y ~ x, data = df) # compute the linear model
+abline(model, col = "black") # draw the model as a blue line
+
